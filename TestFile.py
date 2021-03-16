abb2b5ef01def85e3dcc755267e3256b52d69764
mrandronov88 <72745950+mrandronov88@users.noreply.github.com>
2021-03-16 17:49:23 -0400

    Delete TestFile.py

--- TestFile.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, render_template
-
-test = Blueprint( 'test', __name__ )
-@test.route("/test", methods=['GET'])
-def test_func():
-    print("test!")
-    return render_template('test.html') 
